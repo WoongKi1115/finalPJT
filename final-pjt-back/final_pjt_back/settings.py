c41e9ec73808fc8ee191735b4ac5519e78f58b7a
WoongKi1115 <nwk6638@gmail.com>
2022-11-16 11:00:23 +0900

    1차 업데이트

--- final-pjt-back/final_pjt_back/settings.py
+++ final-pjt-back/final_pjt_back/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'movies',
+    'community',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
