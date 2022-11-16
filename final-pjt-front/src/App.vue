137fceabc79a12e84b398a8d32eb9c37dad91ef3
WoongKi1115 <nwk6638@gmail.com>
2022-11-16 13:51:03 +0900

    vue에 router 입력

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -5,40 +5,44 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+    <div class="d-flex align-center">
+      <v-img
+      alt="Vuetify Logo"
+      class="shrink mr-2"
+      contain
+      src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+      transition="scale-transition"
+      width="40"
+      />
+      
+      <img
+      alt="Vuetify Name"
+      class="shrink mt-1 hidden-sm-and-down"
+      contain
+      min-width="100"
+      src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+      width="100"
         />
       </div>
+      
+      <v-spacer>
 
-      <v-spacer></v-spacer>
-
+      </v-spacer>
+      
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+    <router-link :to="{name:'moviepick'}" class="white--text">추천</router-link>
+  </v-app-bar>
+  
+  <v-main>
 
-    <v-main>
-      <router-view/>
+    <router-view/>
     </v-main>
   </v-app>
 </template>
--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainPageView from '@/views/MainPageView'
+import MoviePickView from '@/views/MoviePickView'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'mainpage',
+    component: MainPageView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/moviepick',
+    name: 'moviepick',
+    component: MoviePickView
   }
 ]
 
