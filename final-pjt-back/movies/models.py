c60edfd24f67f3e0d06951642b120e6a85841ed4
WoongKi1115 <nwk6638@gmail.com>
2022-11-16 15:58:09 +0900

    fixtures 업데이트

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+	name = models.TextField()
+
+
+class Movie(models.Model):
+	adult = models.BooleanField()
+	backdrop_path = models.TextField()
+	genre_ids = models.ManyToManyField(Genre, related_name='movies')
+	original_language = models.CharField(max_length=50)
+	original_title = models.CharField(max_length=50)
+	overview = models.TextField()
+	popularity = models.FloatField()
+	poster_path = models.TextField()
+	release_date = models.DateField()
+	title = models.CharField(max_length=50)
+	video	= models.BooleanField()
+	vote_average = models.FloatField()
+	vote_count = models.FloatField()
