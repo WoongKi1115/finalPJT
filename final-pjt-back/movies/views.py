e223e9445127ec4d564772dff37d6f169d70616e
moooonam <anska77@naver.com>
2022-11-16 17:38:18 +0900

    장르

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -13,6 +13,9 @@ from .models import Movie
 @api_view(['GET'])
 def get_movies_list(request):
     movies = Movie.objects.all()
+    for movie in movies:
+        genres = [genre.name for genre in movie.genres.all()]
+        movie.genre_ids = genres
     serializer = MovieSerializer(movies, many=True)
     return Response(serializer.data)
    
