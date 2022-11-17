a972e926dff1215f0169141978a6aec53389bde3
moooonam <anska77@naver.com>
2022-11-17 16:25:50 +0900

    인기 성곡

--- final-pjt-front/src/views/MainPageView.vue
+++ final-pjt-front/src/views/MainPageView.vue
@@ -1,28 +1,33 @@
 <template>
-	<div>
-		<h1>MainPage</h1>
-		<popularMovie/>
-		<recentMovieitem/>
-		<genreMovieitem/>
-	</div>
+  <div>
+    <h1>MainPage</h1>
+    <popularMovie />
+    <recentMovieitem />
+    <genreMovieitem />
+  </div>
 </template>
 
 <script>
+import popularMovie from "@/components/popularMovie";
+import recentMovieitem from "@/components/recentMovieitem";
+import genreMovieitem from "@/components/genreMovieitem";
 
-import popularMovie from '@/components/popularMovie'
-import recentMovieitem from '@/components/recentMovieitem'
-import genreMovieitem from '@/components/genreMovieitem'
 export default {
-	name:'MainPageView',
-	components:{
-		popularMovie,
-		recentMovieitem,
-		genreMovieitem
-	},
-	
-}
+  name: "MainPageView",
+  components: {
+    popularMovie,
+    recentMovieitem,
+    genreMovieitem,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {},
+
+};
 </script>
 
 <style>
-
 </style>
