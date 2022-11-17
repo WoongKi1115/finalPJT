4991160f1543510f0014a227e766313e821e0bcb
WoongKi1115 <nwk6638@gmail.com>
2022-11-17 14:57:51 +0900

    제발 해줘

---
+++ final-pjt-front/src/components/popularMovie.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<h1>PopularMovie</h1>
+    
+    <!-- <v-carousel
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+    >
+  </v-carousel> -->
+    <!-- <popularMovieItem
+    v-for="popularMovie in popularMovies"
+    :key="popularMovie.pk"
+    :popularMovie='popularMovie'
+    /> -->
+    <p>{{popularMovies}}</p>
+	</div>
+</template>
+
+<script>
+// import popularMovieItem from '@/components/popularMovieItem';
+export default {
+	name:'popularMovie',
+  components:{
+    // popularMovieItem
+  },
+  computed:{
+    popularMovies() {
+      return this.$store.getters.getPopular
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
