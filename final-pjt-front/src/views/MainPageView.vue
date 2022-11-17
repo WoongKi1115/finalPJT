7de8cf7282e346386ef8c891e6bd74c7a95f7a69
WoongKi1115 <nwk6638@gmail.com>
2022-11-17 14:08:56 +0900

    check

--- final-pjt-front/src/views/MainPageView.vue
+++ final-pjt-front/src/views/MainPageView.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+
 import popularMovieitem from '@/components/popularMovieitem'
 import recentMovieitem from '@/components/recentMovieitem'
 import genreMovieitem from '@/components/genreMovieitem'
@@ -19,75 +19,7 @@ export default {
 		recentMovieitem,
 		genreMovieitem
 	},
-	data() {
-    return {
-      movies: [],
-      genres: 
-        {
-          28: "액션",
-          12: "모험",
-          16: "애니메이션",
-          35: "코미디",
-          80: "범죄",
-          99: "다큐멘터리",
-          18: "드라마",
-          10751: "가족",
-          14: "판타지",
-          36: "역사",
-          27: "공포",
-          10402: "음악",
-          9648: "미스터리",
-          10749: "로맨스",
-          878: "SF",
-          10770: "TV 영화",
-          53: "스릴러",
-          10752: "전쟁",
-          37: "서부",
-        },
-    };
-  },
-  computed:{
-    popularMovies() {
-      return this.$store.getters.getPopular
-    },
-    recentMovies() {
-      return this.$store.getters.getRecent
-    }
-  },
-	methods:{
-		getMovies() {
-			axios({
-				method: 'get',
-				url: 'http://127.0.0.1:8000/api/v1/',
-			})
-			.then((res) => {
-				this.movies = res.data
-				this.getGenre()
-				this.$store.state.movies = this.movies
-        // branch test
-        // console.log(this.recentMovies)
-				// console.log(this.$store.state.movies)
-        // console.log(this.$store.getters.getPopular)
-			})
-			.catch((err) => {
-				console.log(err)
-			})
-		},
-		getGenre() {
-      // const movieListLen= movies.length
-      const genresDic = this.genres
-      for (let movie of this.movies) {
-        let newGenre=[]
-        for(let movieGenre of movie.genre_ids){
-          newGenre.push(genresDic[movieGenre])
-        }
-        movie.genre_ids = newGenre
-      }
-		},
-	},
-	created() {
-		this.getMovies()
-	}
+	
 }
 </script>
 
