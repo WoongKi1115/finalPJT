94ad7928b231b25f1fce197aca3d4e252c814d5f
moooonam <anska77@naver.com>
2022-11-18 17:26:43 +0900

    up

--- final-pjt-front/src/views/MoviePickView.vue
+++ final-pjt-front/src/views/MoviePickView.vue
@@ -6,6 +6,7 @@
       <v-dialog
         v-model="dialog"
         hide-overlay
+        height="auto"
         transition="dialog-bottom-transition"
       >
         <template v-slot:activator="{ on, attrs }">
@@ -31,98 +32,254 @@
             </v-toolbar-items>
           </v-toolbar>
           <v-list-item>
-            <v-btn
+            <v-row class="d-flex justify-space-around mb-6">
+              <v-btn
               @click="clicked.clicked1 = !clicked.clicked1"
               :color="
                 clicked.clicked1
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
-              "
+                ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+                  "
               rounded
             >
-              우울한
+              신나는
             </v-btn>
             <v-btn
-              @click="clicked.clicked2 = !clicked.clicked2"
+            @click="clicked.clicked2 = !clicked.clicked2"
               :color="
                 clicked.clicked2
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
-              "
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+                  "
               rounded
             >
-              그리운
+              활기찬
             </v-btn>
             <v-btn
-              @click="clicked.clicked3 = !clicked.clicked3"
+            @click="clicked.clicked3 = !clicked.clicked3"
               :color="
                 clicked.clicked3
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
               "
               rounded
             >
-              낙심한
+              지루한
             </v-btn>
             <v-btn
               @click="clicked.clicked4 = !clicked.clicked4"
               :color="
                 clicked.clicked4
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+            >
+            자랑스러운
+            </v-btn>
+            <v-btn
+              @click="clicked.clicked5 = !clicked.clicked5"
+              :color="
+                clicked.clicked5
+                ? 'deep-purple lighten-1'
+                : 'deep-purple lighten-4'
+                "
+              rounded
+              >
+              자신감넘치는
+            </v-btn>
+            <v-btn
+            @click="clicked.clicked6 = !clicked.clicked6"
+            :color="
+                clicked.clicked6
+                ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+                  "
+              rounded
+              >
+              답답한
+            </v-btn>
+            <v-btn
+              @click="clicked.clicked7 = !clicked.clicked7"
+              :color="
+                clicked.clicked7
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+            >
+            우울한
+            </v-btn>
+            <v-btn
+              @click="clicked.clicked8 = !clicked.clicked8"
+              :color="
+                clicked.clicked8
+                ? 'deep-purple lighten-1'
+                : 'deep-purple lighten-4'
+                "
+              rounded
+            >
+              웃음이필요한
+            </v-btn>
+            <v-btn
+            @click="clicked.clicked9 = !clicked.clicked9"
+            :color="
+                clicked.clicked9
+                ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+              >
+              짜증나는
+            </v-btn>
+            <v-btn
+              @click="clicked.clicked10 = !clicked.clicked10"
+              :color="
+                clicked.clicked10
+                ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+              >
+              통쾌한
+            </v-btn>
+            <v-btn
+            @click="clicked.clicked11 = !clicked.clicked11"
+              :color="
+                clicked.clicked11
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+              >
+              억울한
+            </v-btn>
+            <v-btn
+              @click="clicked.clicked12 = !clicked.clicked12"
+              :color="
+                clicked.clicked12
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+            >
+            행복한
+          </v-btn>
+            <v-btn
+              @click="clicked.clicked13 = !clicked.clicked13"
+              :color="
+                clicked.clicked13
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+              >
+              편안한
+            </v-btn>
+            <v-btn
+            @click="clicked.clicked14 = !clicked.clicked14"
+            :color="
+                clicked.clicked14
+                ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+                  "
+              rounded
+            >
+            속상한
+            </v-btn>
+            <v-btn
+            @click="clicked.clicked15 = !clicked.clicked15"
+            :color="
+                clicked.clicked15
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+            >
+              몽환적인
+            </v-btn>
+            <v-btn
+              @click="clicked.clicked16 = !clicked.clicked16"
+              :color="
+                clicked.clicked16
+                ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+                  "
+              rounded
+              >
+              현실에서 벗어나고픈
+            </v-btn>
+            <v-btn
+            @click="clicked.clicked17 = !clicked.clicked17"
+            :color="
+                clicked.clicked17
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+            >
+              지긋지긋한
+            </v-btn>
+            <v-btn
+              @click="clicked.clicked18 = !clicked.clicked18"
+              :color="
+                clicked.clicked18
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
+              "
+              rounded
+              >
+              설레는
+            </v-btn>
+            <v-btn
+            @click="clicked.clicked19 = !clicked.clicked19"
+              :color="
+                clicked.clicked19
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
               "
               rounded
             >
               외로운
             </v-btn>
             <v-btn
-              @click="clicked.clicked5 = !clicked.clicked5"
+              @click="clicked.clicked20 = !clicked.clicked20"
               :color="
-                clicked.clicked5
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
+                clicked.clicked20
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
               "
               rounded
             >
-              좋은
+            허전한
             </v-btn>
             <v-btn
-              @click="clicked.clicked6 = !clicked.clicked6"
-              :color="
-                clicked.clicked6
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
+            @click="clicked.clicked21 = !clicked.clicked21"
+            :color="
+                clicked.clicked21
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
               "
               rounded
-            >
-              재밌는
+              >
+              더운
             </v-btn>
             <v-btn
-              @click="clicked.clicked7 = !clicked.clicked7"
+              @click="clicked.clicked22 = !clicked.clicked22"
               :color="
-                clicked.clicked7
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
+                clicked.clicked22
+                  ? 'deep-purple lighten-1'
+                  : 'deep-purple lighten-4'
               "
               rounded
             >
-              벅찬
-            </v-btn>
-            <v-btn
-              @click="clicked.clicked8 = !clicked.clicked8"
-              :color="
-                clicked.clicked8
-                  ? 'deep-purple darken-1'
-                  : 'deep-purple lighten-3'
-              "
-              rounded
-            >
-              활기찬
-            </v-btn>
-          </v-list-item>
-        </v-card>
-      </v-dialog>
-    </v-row>
+            흥분되는
+          </v-btn>
+        </v-row>
+        </v-list-item>
+      </v-card>
+    </v-dialog>
+  </v-row>
     <br />
     <br />
     <moviePickitem />
@@ -131,7 +288,7 @@
 
 <script>
 import moviePickitem from "@/components/moviePickitem";
-import _ from "lodash"
+import _ from "lodash";
 export default {
   name: "MoviePickView",
   methods: {
@@ -171,11 +328,10 @@ export default {
       for (let id in this.moodsCount) {
         let count = this.moodsCount[id];
         if (count) {
-          console.log(count)
-          const recommendedMovie =  _.cloneDeep(this.classifyMovie[id])
-          recommendedMovie.splice(count)
-          for (let movie of recommendedMovie){
-            this.recommendMovie.push(movie)
+          const recommendedMovie = _.cloneDeep(this.classifyMovie[id]);
+          recommendedMovie.splice(count);
+          for (let movie of recommendedMovie) {
+            this.recommendMovie.push(movie);
           }
         }
       }
@@ -204,14 +360,40 @@ export default {
         clicked6: false,
         clicked7: false,
         clicked8: false,
+        clicked9: false,
+        clicked10: false,
+        clicked11: false,
+        clicked12: false,
+        clicked13: false,
+        clicked14: false,
+        clicked15: false,
+        clicked16: false,
+        clicked17: false,
+        clicked18: false,
+        clicked19: false,
+        clicked20: false,
+        clicked21: false,
+        clicked22: false,
       },
       moods: {
-        35: ["clicked1", "clicked2", "clicked3", "clicked4"],
-        28: ["clicked5", "clicked6", "clicked7", "clicked8"],
+        28: ["clicked1", "clicked2", "clicked3"],
+        12: ["clicked4", "clicked5", "clicked6"],
+        35: ["clicked7", "clicked8", "clicked9"],
+        80: ["clicked10", "clicked11"],
+        10751: ["clicked12", "clicked13", "clicked14"],
+        14: ["clicked15", "clicked16", "clicked17"],
+        10749: ["clicked18", "clicked19", "clicked20"],
+        53: ["clicked21", "clicked22"],
       },
       moodsCount: {
-        35: 0,
         28: 0,
+        12: 0,
+        35: 0,
+        80: 0,
+        10751: 0,
+        14: 0,
+        10749: 0,
+        53: 0,
       },
       clickedList: [],
       pickedMoods: [],
