b53f499f9267a50842cbf5a32a104a0cf582570c
WoongKi1115 <nwk6638@gmail.com>
2022-11-22 00:11:55 +0900

    community detail

--- final-pjt-front/src/views/CommunityDetailView.vue
+++ final-pjt-front/src/views/CommunityDetailView.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
-    <h1>Detail</h1>
-    <p>번호 : {{ article?.id }}</p>
-    <p>제목 : {{ article?.title }}</p>
-    <p>내용 : {{ article?.content }}</p>
+  <div class="detailpage">
+    <v-btn
+      style="float:right"
+      class=""
+      fab
+      dark
+      x-small
+      color="gray"
+      @click="goCommunity"
+    >
+    X
+    </v-btn>
+    <br>
+    <h1>{{ article?.title }}</h1>
+    <div style="padding-right:100px; padding-left:100px;">
+    <div style="text-align:right; font-size:16px;">작성자 : {{article.username}}</div>
+    <div style="text-align:right; font-size:14px;">작성시간 : {{article.created_at | moment('YYYY-MM-DD HH:mm:ss')}}</div>
+    <div style="text-align:left; font-size:19px;" class="mt-10 mb-15">내용 : {{ article?.content }}</div>
+    </div>
     <div class="comment">
       <v-text-field
         label="댓글"
@@ -11,6 +25,7 @@
         filled
         rounded
         dense
+        style="background-color:white;"
         @keyup.enter="createComment"
       ></v-text-field>
     </div>
@@ -30,6 +45,9 @@ export default {
   },
   computed: {},
   methods: {
+    goCommunity() {
+      this.$router.push({name:"community"})
+    },
     getArticle(articleId) {
       axios({
         method: "get",
@@ -72,6 +90,17 @@ export default {
 
 <style>
 .comment {
-  background-color: white;
+
+
+  
+}
+.detailpage{
+  width: 80%;
+  margin : auto;
+  margin-top: 90px;
+  border-radius: 20px;
+  padding: 25px;
+  background-color: azure;
+  color: black;
 }
 </style>
