e7f26df9e8f8d76b7cb4d4e0af2f2f7daf5efb1f
moooonam <anska77@naver.com>
2022-11-22 11:41:44 +0900

    up

--- final-pjt-front/src/components/communityItem.vue
+++ final-pjt-front/src/components/communityItem.vue
@@ -1,37 +1,57 @@
 <template>
-	<div @click="goDetail(article.id)" class="selectArticle board_list_body pa-4 d-flex align-items-center" :class="{'id_hol':checkId}">
-		<div class="articles_id" :class={}>{{ article.id }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
-          <div class="articles_title">{{ article.title }}</div>
-          <div class="articles_username">{{ article.username }}</div>
-          <div class="articles_created_at">{{article.created_at | moment('YYYY-MM-DD HH:mm:ss')}}</div>
-          <div class="articles_views"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;댓글</div>
-	</div>
-
+  <div
+    @click="goDetail(article.id)"
+    class="selectArticle board_list_body pa-4 d-flex align-items-center"
+    :class="{ id_hol: checkId }"
+  >
+    <div class="articles_id" :class="{}">
+      {{ article.id }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+    <div class="articles_title">{{ article.title }} ({{ commentsLengh }})</div>
+    <div class="articles_username">{{ article.username }}</div>
+    <div class="articles_created_at">
+      {{ article.created_at | moment("YYYY-MM-DD HH:mm:ss") }}
+    </div>
+    <div class="articles_views">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;댓글</div>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import vueMoment from 'vue-moment'
-Vue.use(vueMoment)
+import Vue from "vue";
+import vueMoment from "vue-moment";
+import axios from "axios";
+const API_URL = "http://127.0.0.1:8000";
+
+Vue.use(vueMoment);
 export default {
-	name:'communityItem',
-	data() {
-		return {
-			datetime:null,
-			checkId:false,
-		}
-	},
-	props:{
-		article:Object
-	},
-	methods:{
-		goDetail(id) {
-			this.$router.push({name:'communityDetail', params:{id}})
-		},
-	},
-	created() {
-	}
-}
+  name: "communityItem",
+  data() {
+    return {
+      datetime: null,
+      checkId: false,
+      commentsLengh: 0,
+    };
+  },
+  props: {
+    article: Object,
+  },
+  methods: {
+    goDetail(id) {
+      this.$router.push({ name: "communityDetail", params: { id } });
+    },
+    getCommentLengh() {
+      axios({
+        method: "get",
+        url: `${API_URL}/community/${this.article.id}/comment_list_create/`,
+      }).then((res) => {
+        this.commentsLengh = res.data.length;
+      });
+    },
+  },
+  created() {
+    this.getCommentLengh()
+  },
+};
 </script>
 
 <style>
@@ -40,15 +60,15 @@ export default {
 }
 
 .board_list_body {
-	display: inline-block;
-	font-size:19px;
-	border-bottom: 1px solid;
+  display: inline-block;
+  font-size: 19px;
+  border-bottom: 1px solid;
 }
 .id_hol {
-	background-color: gary;
+  background-color: gary;
 }
 .id_hod {
-	background-color: black;
+  background-color: black;
 }
 
 .articles_id {
